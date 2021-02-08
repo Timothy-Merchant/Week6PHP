9266111e0c2ea5d3fe8f20785e17861c93a946dc
Tim <ryalin1286@gmail.com>
2021-02-08 15:52:18 +0000

    Completed challenge 10

---
+++ 02-arrays-functions/10average.php
@@ -0,0 +1,30 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+// Foreach method
+// function average($values)
+// {
+//     $total = 0;
+
+//     foreach ($values as $value) {
+//         $total += $value;
+//     }
+
+//     return $total / count($values);
+// }
+
+// Collect method
+function average($values)
+{    
+    $numbers = collect($values);
+    $total = $numbers->reduce(fn ($acc, $val) => $acc + $val, 0);
+    return $total / count($values);
+}
+
+dump(
+    average([2, 3, 4, 5, 6]), // 4
+    average([2, 3]), // 2.5
+    average([10, 30]), // 20
+    average([-4, -8, -9]), // -7
+);
