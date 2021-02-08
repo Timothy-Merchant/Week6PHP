ccc4643b581285921ff9ab9bad9e8abb9a8b87d4
Tim <ryalin1286@gmail.com>
2021-02-08 11:55:20 +0000

    Completed challenge 02

---
+++ 01-basic-php/02Concatenation.php
@@ -0,0 +1,14 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+$string1 = "I";
+$string2 = "like";
+$string3 = "to";
+$string4 = "boogie";
+$string5 = "boogie";
+
+$joined = $string1 . " " . $string2 . " " . $string3 . " " . $string4 . " " . $string5;
+$joined2 = "{$string1} {$string2} {$string3} {$string4} {$string5}";
+dump($joined); // "I like to boogie boogie"
+dump($joined2); // "I like to boogie boogie"
