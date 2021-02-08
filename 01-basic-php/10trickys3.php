6ee42d53693d928fd2daf36de21c5637459a2693
Tim <ryalin1286@gmail.com>
2021-02-08 15:08:59 +0000

    setup files for afternoon challenges

--- 01-basic-php/10trickys3.php
+++ 01-basic-php/10trickys3.php
@@ -4,11 +4,12 @@ require __DIR__ . "/vendor/autoload.php";
 
 $report = "";
 
-for ($timestables = 1; $timestables <= 15; $timestables += 1) {
-    for ($i = 1; $i <= 15; $i++) {
-        $entry = $timestables * $i;
+for ($timestable = 1; $timestable <= 15; $timestable += 1) {
+    for ($i = 1; $i <= 15; $i += 1) {
+        $entry = $timestable * $i;
         $report .= "{$entry}\t";
     }
+
     $report .= "\n";
 }
 
