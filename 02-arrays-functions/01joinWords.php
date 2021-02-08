8f786c113651f817ef7ee2d407eef3c53649a078
Tim <ryalin1286@gmail.com>
2021-02-08 15:12:03 +0000

    Challenge 01 complete

--- 02-arrays-functions/01joinWords.php
+++ 02-arrays-functions/01joinWords.php
@@ -2,3 +2,10 @@
 
 require __DIR__ . "/vendor/autoload.php";
 
+function joinWords($firstWord, $secondWord)
+{
+    return $firstWord . " " . $secondWord;
+};
+
+dump(joinWords("fish", "sticks")); // "fish sticks"
+dump(joinWords("hello", "world")); // "hello world"
