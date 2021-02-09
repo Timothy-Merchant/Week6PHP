9652554a5ef527ad7541dfc6f8e3094abb55777b
Tim <ryalin1286@gmail.com>
2021-02-09 12:18:59 +0000

    Completed challenge 04

---
+++ 03-regex/04.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+function phoneNumber($phoneNumber)
+{
+    return preg_match('/^[0-9-]*$/', $phoneNumber) === 1;
+}
+
+dump(phoneNumber("blah")); // false
+dump(phoneNumber("12-323-5-344")); // true
+dump(phoneNumber("12-34-a334-43")); // false
+dump(phoneNumber("--123----23123")); // true
